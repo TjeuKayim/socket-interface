572b66f82460dd36e288ddf137cba7e4f3b1e780
Tjeu Kayim <tjeu.kayim@gmail.com>
2018-04-11 20:35:09 +0200

    Add unit-test for invalid protocols

---
+++ src/test/java/com/github/tjeukayim/socketinterface/InvalidProtocolTest.java
@@ -0,0 +1,45 @@
+package com.github.tjeukayim.socketinterface;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.function.Consumer;
+import org.junit.jupiter.api.Test;
+
+public class InvalidProtocolTest {
+
+  private Consumer<Message> consumerMock = m -> {
+  };
+
+  @Test
+  void noInterface() {
+    assertThrows(IllegalArgumentException.class, () ->
+        MessageSender.create(String.class, consumerMock));
+  }
+
+  interface A {
+
+    void hello();
+  }
+
+  @Test
+  void returnVoid() {
+    assertThrows(IllegalArgumentException.class, () ->
+        MessageSender.create(A.class, consumerMock));
+  }
+
+  interface B {
+
+    C c();
+  }
+
+  interface C {
+
+    boolean hello();
+  }
+
+  @Test
+  void invalidEndpoint() {
+    assertThrows(IllegalArgumentException.class, () ->
+        MessageSender.create(B.class, consumerMock));
+  }
+}
